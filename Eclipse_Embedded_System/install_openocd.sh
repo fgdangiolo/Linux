85e6fca803016b1e38bdad9ccf78c56407d5c390
fgdangiolo <nubeingenieria@gmail.com>
2018-10-11 12:27:02 -0300

    Uploaded

---
+++ Eclipse_Embedded_System/install_openocd.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This bash file update and upgrade the system
+
+wget https://github.com/gnu-mcu-eclipse/openocd/releases/download/v0.10.0-8-20180512/gnu-mcu-eclipse-openocd-0.10.0-8-20180512-1921-centos64.tgz
+
+sudo mkdir -p ${HOME}/opt
+cd ${HOME}/opt
+sudo tar xvf ~/gnu-mcu-eclipse-openocd-0.10.0-8-20180512-1921-centos64.tgz
+sudo chmod -R -w ${HOME}/opt/gnu-mcu-eclipse/openocd/0.10.0-8-20180512-1921
+
+echo "========================"
+echo "Check if OpenOCD starts"
+echo "========================"
+
+sudo ${HOME}/opt/gnu-mcu-eclipse/openocd/0.10.0-8-20180512-1921/bin/openocd --version
+
+echo "========================"
+echo "Testing..."
+echo "========================"
+
+sudo ${HOME}/opt/gnu-mcu-eclipse/openocd/0.10.0-8-20180512-1921/bin/openocd \
+  -f board/stm32f4discovery.cfg
+
