99bfdfe1f8060ce898c7e478d94e5cb5f1c95edb
fdangiolo <dangiolofg@gmail.com>
2018-10-11 23:47:45 -0300

    Uploaded

---
+++ Eclipse_Embedded_System/install_eclipse.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This bash file update and upgrade the system
+
+echo "====================================================="
+echo "Downloading GNU MCU Eclipse IDE for C/C++ Developers"
+echo "====================================================="
+
+wget https://github.com/gnu-mcu-eclipse/org.eclipse.epp.packages/releases/download/v4.4.2-20180930-2018-09/20180930-0922-gnumcueclipse-4.4.2-2018-09-linux.gtk.x86_64.tar.gz.sha
+
+
+
+
+
